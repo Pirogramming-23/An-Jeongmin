7a294c383b61753256356a1c5ace72d9bcfaec05
Annie <ajm010402@gmail.com>
2025-07-04 19:04:00 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,19 +1,24 @@
 num = 0
+turn ='A'
 
 while True:
-    nums = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    while True:
+        
+        nums = input(f"player{turn} - 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-    if not nums.isdigit():
-        print("정수를 입력하세요")
-        continue
+        if not nums.isdigit():
+            print("정수를 입력하세요")
+            continue
 
-    nums = int(nums)
-    if nums not in [1,2,3]:
-        print("1,2,3 중 하나를 입력하세요")
-        continue
+        nums = int(nums)
+        if nums not in [1,2,3]:
+            print("1,2,3 중 하나를 입력하세요")
+            continue
 
-    break
+        break
 
-for i in range(1, nums+1):
-    num+=1
-    print(f"playerA : {num}")
+    for i in range(nums):
+        num+=1
+        print(f"player{turn}: {num}")
+    
+    turn = 'B' if turn == 'A' else 'A'
