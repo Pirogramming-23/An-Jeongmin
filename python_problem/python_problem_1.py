a9c6cbf2365bda4c9972cc9dac34ae8c023ea045
Annie <ajm010402@gmail.com>
2025-07-04 19:43:10 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,32 @@
-num = 0
-turn ='A'
+def brGame():
+    num = 0
+    turn ='A'
 
-while num<31:
-    while True:
-        
-        nums = input(f"player{turn} - 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    while num<31:
+        while True:
+            
+            nums = input(f"player{turn} - 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-        if not nums.isdigit():
-            print("정수를 입력하세요")
-            continue
+            if not nums.isdigit():
+                print("정수를 입력하세요")
+                continue
 
-        nums = int(nums)
-        if nums not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-            continue
+            nums = int(nums)
+            if nums not in [1,2,3]:
+                print("1,2,3 중 하나를 입력하세요")
+                continue
 
-        break
+            break
 
-    for i in range(nums):
-        num+=1
-        print(f"player{turn}: {num}")
-        
-        if num >= 31:
-            winner = 'B' if turn == 'A' else 'A'
-            print(f"player{winner} win!")
-            exit()
+        for i in range(nums):
+            num+=1
+            print(f"player{turn}: {num}")
+            
+            if num >= 31:
+                winner = 'B' if turn == 'A' else 'A'
+                return winner
 
-    turn = 'B' if turn == 'A' else 'A'
+        turn = 'B' if turn == 'A' else 'A'
 
+winner = brGame()
+print(f"player{winner} win!")
